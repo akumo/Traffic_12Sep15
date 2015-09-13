35e3250e07fb256068cf105c7934191b3902efd4
akumo <akumo@yahoo.com>
2015-09-13 10:31:08 +0700

    Commit from Graphic #2

--- app/src/main/java/com/example/akumo/traffic/MainActivity.java
+++ app/src/main/java/com/example/akumo/traffic/MainActivity.java
@@ -4,14 +4,36 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    //Explicit
+    private ListView trafficListView;
+    private Button aboutMeButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    
-}
+        //Initial Widget
+        initialWidget();
+
+
+
+
+    } // Main Method
+
+    private void initialWidget() {
+
+        trafficListView = (ListView) findViewById(R.id.listView);
+        aboutMeButton = (Button) findViewById(R.id.button);
+
+    } // initialWidget
+
+
+} // Main Class
