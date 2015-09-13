8a7f64593a413b35a0e036046118bbc0112be40e
akumo <akumo@yahoo.com>
2015-09-13 14:27:43 +0700

    Commit from Graphic #2

--- app/src/main/java/com/example/akumo/traffic/MainActivity.java
+++ app/src/main/java/com/example/akumo/traffic/MainActivity.java
@@ -37,7 +37,56 @@ public class MainActivity extends AppCompatActivity {
 
     private void createListView() {
 
+        //Setup Icon
+        int intIcon[] = new int[20];
+        intIcon[0] = R.drawable.traffic_01;
+        intIcon[1] = R.drawable.traffic_02;
+        intIcon[2] = R.drawable.traffic_03;
+        intIcon[3] = R.drawable.traffic_04;
+        intIcon[4] = R.drawable.traffic_05;
+        intIcon[5] = R.drawable.traffic_06;
+        intIcon[6] = R.drawable.traffic_07;
+        intIcon[7] = R.drawable.traffic_08;
+        intIcon[8] = R.drawable.traffic_09;
+        intIcon[9] = R.drawable.traffic_10;
+        intIcon[10] = R.drawable.traffic_11;
+        intIcon[11] = R.drawable.traffic_12;
+        intIcon[12] = R.drawable.traffic_13;
+        intIcon[13] = R.drawable.traffic_14;
+        intIcon[14] = R.drawable.traffic_15;
+        intIcon[15] = R.drawable.traffic_16;
+        intIcon[16] = R.drawable.traffic_17;
+        intIcon[17] = R.drawable.traffic_18;
+        intIcon[18] = R.drawable.traffic_19;
+        intIcon[19] = R.drawable.traffic_20;
+
+        //Setup Title
+        String[] strTitle = new String[20];
+        strTitle[0] = "ป้ายจราจรที่ 1";
+        strTitle[1] = "ป้ายจราจรที่ 2";
+        strTitle[2] = "ป้ายจราจรที่ 3";
+        strTitle[3] = "ป้ายจราจรที่ 4";
+        strTitle[4] = "ป้ายจราจรที่ 5";
+        strTitle[5] = "ป้ายจราจรที่ 6";
+        strTitle[6] = "ป้ายจราจรที่ 7";
+        strTitle[7] = "ป้ายจราจรที่ 8";
+        strTitle[8] = "ป้ายจราจรที่ 9";
+        strTitle[9] = "ป้ายจราจรที่ 10";
+        strTitle[10] = "ป้ายจราจรที่ 11";
+        strTitle[11] = "ป้ายจราจรที่ 12";
+        strTitle[12] = "ป้ายจราจรที่ 13";
+        strTitle[13] = "ป้ายจราจรที่ 14";
+        strTitle[14] = "ป้ายจราจรที่ 15";
+        strTitle[15] = "ป้ายจราจรที่ 16";
+        strTitle[16] = "ป้ายจราจรที่ 17";
+        strTitle[17] = "ป้ายจราจรที่ 18";
+        strTitle[18] = "ป้ายจราจรที่ 19";
+        strTitle[19] = "ป้ายจราจรที่ 20";
+
+        MyAdapter objMyAdapter = new MyAdapter(MainActivity.this, intIcon, strTitle);
+        trafficListView.setAdapter(objMyAdapter);
         
+
     } // createListView
 
     private void buttonController() {
