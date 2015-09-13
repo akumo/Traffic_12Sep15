c2df9815331d55aedc80a4ef6deebf2659b92830
akumo <akumo@yahoo.com>
2015-09-13 11:02:25 +0700

    Commit from Graphic #2

--- app/src/main/java/com/example/akumo/traffic/MainActivity.java
+++ app/src/main/java/com/example/akumo/traffic/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.akumo.traffic;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -23,17 +25,34 @@ public class MainActivity extends AppCompatActivity {
         //Initial Widget
         initialWidget();
 
-
+        //Button Controller
+        buttonController();
 
 
     } // Main Method
+
+    private void buttonController() {
+
+        aboutMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Sound Effect
+                MediaPlayer buttonMediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.effect_btn_shut);
+                buttonMediaPlayer.start();
+
+
+            } // event จะทำงานเมื่อมีการกดปุ่มนี้
+        });
+
+    } // buttonController
 
     private void initialWidget() {
 
         trafficListView = (ListView) findViewById(R.id.listView);
         aboutMeButton = (Button) findViewById(R.id.button);
 
-    } // initialWidget
+    } // initialWidget  bind ตัวแปรกับ object
 
 
 } // Main Class
